c8110678449b25ddbaab4976c86f786ea06ea5bc
Stephens <Keagan.stephens@icloud.com>
2023-12-16 07:26:24 +0200

    added development notifications while cleaning up the ui. Searching for cv also work, but search button is non-working

--- src/app/search-field/search-field.component.ts
+++ src/app/search-field/search-field.component.ts
@@ -1,16 +1,30 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ElementRef, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-field',
   templateUrl: './search-field.component.html',
   styleUrls: ['./search-field.component.css']
 })
-export class SearchFieldComponent {
+export class SearchFieldComponent implements AfterViewInit {
   @Output() searchInput = new EventEmitter<string>();
+  @ViewChildren('underDevelopmentButtons') underDevelopmentButtons!: QueryList<HTMLButtonElement>;
+
+  ngAfterViewInit() {
+    // Wait for the view to be initialized
+  }
 
   search(event: any) {
     const inputValue = event.target.value.toLowerCase();
     this.searchInput.emit(inputValue);
   }
-}
 
+  // Listen for click events on buttons with the "underDevelopment" class
+  handleClick(button: HTMLButtonElement) {
+    const buttonText = button.textContent?.trim() || '';
+    this.showPopup(`This feature is still under development: ${buttonText}`);
+  }
+
+  private showPopup(message: string) {
+    alert(message);
+  }
+}
